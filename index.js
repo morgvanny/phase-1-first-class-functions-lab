5767031e248df44c6bf7ee5c2c12f8508b8bff87
Morgan VanYperen <morgvanny@gmail.com>
2022-07-25 14:34:33 -0700

    commit tree 8c67cea8e6896c9947872c77a767cdf5bf6c4e36

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (driverNames) {
+  return driverNames.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function (driverNames) {
+  return driverNames.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num) {
+  return function (fare) {
+    return num * fare;
+  };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, driverSelectorFn) {
+  return driverSelectorFn(arrayOfDrivers);
+}
